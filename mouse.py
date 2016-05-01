4fd14ca264953f617aca258b9621b1b211eba07e
Wade Mauger <Wade Mauger>
2016-05-01 15:45:25 -0400

    Scale mouse output to display resolution

--- mouse.py
+++ mouse.py
@@ -1,7 +1,26 @@
 import win32api
 import sys
+from win32api import GetSystemMetrics
+
+displayWidth = 1920 #GetSystemMetrics(0)
+displayHeight = 1080 #GetSystemMetrics(1)
+
+#the webcam we used was 640 x 480 
+
+camResHeight = 640
+camResWidth = 480
+
+print("DH= ", displayHeight)
+print("DW= ", displayWidth)
+print("CH= ", camResHeight)
+print("CW= ", camResWidth)
 
 for line in sys.stdin:
-    newX, newY = line.split()
-    win32api.SetCursorPos((int(newX), int(newY)))
+    newY, newX = line.split()
+    newX = int(newX)
+    newY = int(newY)
+
+    #print(int(newX * (displayWidth/camResWidth)), int(newY * (displayHeight/camResHeight)))
+    #sys.stdout.flush()
+    win32api.SetCursorPos((int(newX * (displayWidth/camResWidth)), int(newY * (displayHeight/camResHeight))))
  
